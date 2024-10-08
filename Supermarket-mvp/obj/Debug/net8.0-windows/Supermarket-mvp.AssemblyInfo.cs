c2f5871616c482473fed8f56f981669a2b5e9f9a
juanposada25 <juandiegoposada245@gmail.com>
2024-10-08 12:25:23 -0500

    ICustomerRepository

--- Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af385c4151153ac9310483b4accdeb10de87e9ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f27aac68bf0434bea5e47ca0fc29f3ab4b0c7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
