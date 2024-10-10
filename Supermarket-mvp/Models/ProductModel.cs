138ec82f873d8adacc3ac8f08282357eb0d754fb
juanposada25 <juandiegoposada245@gmail.com>
2024-10-09 22:33:28 -0500

    product model repository y iview

---
+++ Supermarket-mvp/Models/ProductModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class ProductModel
+    {
+        [DisplayName("Product Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Product Name")]
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(80, MinimumLength = 3, ErrorMessage = "Product Name must be between 3 and 80 Charaters")]
+        public string Name { get; set; }
+
+        [DisplayName("Product Price")]
+        [Required(ErrorMessage = "Product Price is required")]
+        public int Price { get; set; }
+
+        [DisplayName("Product Stock")]
+        [Required(ErrorMessage = "Product Stock is required")]
+        public int Stock { get; set; }
+
+        [DisplayName("Product Categorie_Id")]
+        [Required(ErrorMessage = "Product Categorie_Id is required")]
+        public int Categorie_Id { get; set; }
+
+
+    }
+}
--- Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ecdf82be78f4e9f80b50bbc0709199ca806429e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829405110bd3e4f45be881e846518592ece80119")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
